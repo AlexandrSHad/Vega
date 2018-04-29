4c7db498bc7b79e07fc33f1aac9810ea548e7f90
Alexandr SHad <shadurskij@gmail.com>
2018-04-29 22:13:59 +0300

    installed AutoMapper

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,8 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper();
+            
             services.AddDbContext<VegaDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("Default"))
             );
