20387fe3e03ab5a6169ad5f951ac7140241df770
Alexandr SHad <shadurskij@gmail.com>
2018-04-30 22:46:54 +0300

    add vehicles controller, implement mapping for VehicleResource

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,25 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using vega.Controllers.Resources;
+using vega.Models;
+
+namespace vega.Controllers
+{
+    [Route("/api/vehicles")]
+    public class VehiclesController : Controller
+    {
+        private readonly IMapper _mapper;
+
+        public VehiclesController(IMapper mapper)
+        {
+            this._mapper = mapper;
+        }
+
+        [HttpPost()]
+        public IActionResult CreateVehicle([FromBody] VehicleResource vehicleResource)
+        {
+            var vehicle = _mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            return Ok(vehicle);
+        }
+    }
+}
