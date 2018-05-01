bac3da9364ef8941956e4b380380864a3ad3a9a4
Alexandr SHad <shadurskij@gmail.com>
2018-05-01 21:24:54 +0300

    implement server-side validation for create vehicle

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -23,6 +23,26 @@ namespace vega.Controllers
         [HttpPost()]
         public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
         {
+            // only for input validation
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // // Model Id validation is required if we have to build an public API,
+            // // but in this case it is an extra noise, because our Angular client
+            // // should not send icorrect Id.
+            // // The same is about Id's for each feature.
+            // var model = await _context.Models.FindAsync(vehicleResource.ModelId);
+            //
+            // if (model == null)
+            // {
+            //     ModelState.AddModelError("ModelId", "Invalid model Id.");
+            //     return BadRequest(ModelState);
+            // }
+
+            // // for business logic validation
+            // if (<condition>)
+            //     ModelState.AddModelError("", "<error>");
+
             var vehicle = _mapper.Map<VehicleResource, Vehicle>(vehicleResource);
             vehicle.LastUpdate = DateTime.Now;
 
