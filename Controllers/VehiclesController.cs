903f198ad2814a4a96690718e01af282c1cf9c2a
Alexandr SHad <shadurskij@gmail.com>
2018-05-31 23:59:46 +0300

    implemet API for delete and get vehicle

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -74,5 +74,34 @@ namespace vega.Controllers
 
             return Ok(result);
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle = await _context.Vehicles.FindAsync(id);
+
+            if (vehicle == null)
+                return NotFound();
+
+            _context.Remove(vehicle);
+            await _context.SaveChangesAsync();
+
+            return Ok(id);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id)
+        {
+            var vehicle = await _context.Vehicles
+                .Include(v => v.Features)
+                .SingleOrDefaultAsync(v => v.Id == id);
+
+            if (vehicle == null)
+                return NotFound();
+
+            var vehicleResource = _mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(vehicleResource);
+        }
     }
 }
