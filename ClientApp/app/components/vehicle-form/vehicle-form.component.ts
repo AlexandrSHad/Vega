bae1b4799e0ad2754e3e7c8fe7de7ff1d47a46e5
Alexandr SHad <shadurskij@gmail.com>
2018-07-19 23:53:56 +0300

    edit vehicle and route to home page if not found (edit form is not fully functional yet)

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastyService } from 'ng2-toasty';
 import { VehicleService } from './../services/vehicle.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-vehicle-form',
@@ -18,9 +19,27 @@ export class VehicleFormComponent implements OnInit {
   };
   
   constructor(
-    private vehicleService: VehicleService) { }
+    private route: ActivatedRoute,
+    private router: Router,
+    private vehicleService: VehicleService,
+    private toastyService: ToastyService ) {
+
+      route.params.subscribe(p => {
+        this.vehicle.id = +p['id'];
+      });
+
+    }
 
   ngOnInit() {
+    this.vehicleService.getVehicle(this.vehicle.id)
+      .subscribe(
+        v => this.vehicle = v,
+        err => {
+          if (err.status == 404)
+            this.router.navigate(['/home']); // in real app you can use something like ['/not-found'] route and appropriate component
+        }
+      );
+
     this.vehicleService.getMakes().subscribe(makes => this.makes = makes);
     this.vehicleService.getFeatures().subscribe(features => this.features = features);
   }
