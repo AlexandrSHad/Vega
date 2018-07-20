849e62cd493d450fc212a8f7721b45dc84e4bdcf
Alexandr SHad <shadurskij@gmail.com>
2018-07-21 02:04:35 +0300

    iplement vehicle deletion

--- ClientApp/app/services/vehicle.service.ts
+++ ClientApp/app/services/vehicle.service.ts
@@ -5,14 +5,14 @@ import 'rxjs/add/operator/map'
 
 @Injectable()
 export class VehicleService {
-
+  
   constructor(private http: Http) { }
-
+  
   getMakes() {
     return this.http.get('/api/makes')
       .map(res => res.json());
   }
-
+  
   getFeatures() {
     return this.http.get("/api/features")
       .map(res => res.json());
@@ -22,15 +22,20 @@ export class VehicleService {
     return this.http.post("/api/vehicles", vehicle)
       .map(res => res.json());
   }
-
+  
   getVehicle(id: any) {
     return this.http.get('/api/vehicles/' + id)
       .map(res => res.json());
   }
-
+  
   update(vehicle: SaveVehicle) {
     return this.http.put('/api/vehicles/' + vehicle.id, vehicle)
-      .map(res => res.json())
+      .map(res => res.json());
   }
 
+  delete(id: any) {
+    return this.http.delete('/api/vehicles/' + id)
+      .map(res => res.json());
+  }
+  
 }
