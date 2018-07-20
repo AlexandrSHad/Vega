144d3881a46e69c7a80d3f1dd74398be0ee80fd6
Alexandr SHad <shadurskij@gmail.com>
2018-07-21 01:46:04 +0300

    show toast notification on vehicle updated or created

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -99,20 +99,19 @@ export class VehicleFormComponent implements OnInit {
   }
 
   submit( ) {
-    if (this.vehicle.id)
-      this.vehicleService.update(this.vehicle)
-        .subscribe(x => {
-          this.toastyService.success({
-            title: 'Success',
-            msg: 'The vehicle was successfully updated.',
-            theme: 'bootsrap',
-            showClose: true,
-            timeout: 5000
-          })
-        });
-    else
-      this.vehicleService.create(this.vehicle)
-        .subscribe(x => console.log(x));
+    var result$ = (this.vehicle.id) ?
+      this.vehicleService.update(this.vehicle) :
+      this.vehicleService.create(this.vehicle);
+
+    result$.subscribe(x => {
+      this.toastyService.success({
+        title: 'Success',
+        msg: 'The vehicle was successfully saved.',
+        theme: 'bootstrap',
+        showClose: true,
+        timeout: 5000
+      });
+    });
   }
 
 }
