a6f39c36e9fa91ed943082d1014a399a91c00db1
Alexandr SHad <shadurskij@gmail.com>
2018-07-26 23:40:41 +0300

    WIP: added endpoint for upload images

---
+++ Controllers/PhotosController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using vega.Controllers.Resources;
+using vega.Core;
+using vega.Core.Models;
+
+namespace vega.Controllers
+{
+    [Route("/api/vehicles/{vehicleId}/photos")]
+    public class PhotosController : Controller
+    {
+        private readonly IHostingEnvironment _host;
+        private readonly IMapper _mapper;
+        private readonly IVehicleRepository _vehicleRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PhotosController(IHostingEnvironment host, IMapper mapper, IVehicleRepository vehicleRepository, IUnitOfWork unitOfWork)
+        {
+            _host = host;
+            _mapper = mapper;
+            _vehicleRepository = vehicleRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(int vehicleId, IFormFile file)
+        {
+            var vehicle = await _vehicleRepository.GetVehicle(vehicleId, includeRelated: false);
+            if (vehicle == null)
+                return NotFound();
+
+            var uploadsFolderPath = Path.Combine(_host.WebRootPath, "uploads");
+            if (!Directory.Exists(uploadsFolderPath))
+                Directory.CreateDirectory(uploadsFolderPath);
+
+            var fileName = Guid.NewGuid().ToString() + "." + Path.GetExtension(file.FileName);
+            var filePath = Path.Combine(uploadsFolderPath, fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            // TODO: create thumbnail for image
+
+            var photo = new Photo { FileName = fileName };
+            vehicle.Photos.Add(photo);
+            await _unitOfWork.CompliteAsync();
+
+            return Ok(_mapper.Map<Photo, PhotoResource>(photo));
+        }
+    }
+}
